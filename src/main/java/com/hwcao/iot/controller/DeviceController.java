9d9ce0e738149192ffc34038e14baea8c91d3cc6
guyaren <1056760157@qq.com>
2019-07-07 17:21:20 +0800

    实现设备查询导出为Excel

--- src/main/java/com/hwcao/iot/controller/DeviceController.java
+++ src/main/java/com/hwcao/iot/controller/DeviceController.java
@@ -1,12 +1,19 @@
 package com.hwcao.iot.controller;
 
+import com.hwcao.iot.dto.device.DeviceExcelDTO;
 import com.hwcao.iot.dto.device.DeviceReqDTO;
 import com.hwcao.iot.entity.Result;
 import com.hwcao.iot.entity.device.Device;
 import com.hwcao.iot.service.DeviceService;
+import com.hwcao.iot.util.ExcelUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -40,4 +47,23 @@ public class DeviceController {
     public Result<List<Device>> findAllByUser(@RequestParam("userId") Long userId ){
         return deviceService.findAllByUser(userId);
     }
+
+    /**
+     * Excel表格导出接口
+     * http://localhost:8080/ExcelDownload
+     * @param response response对象
+     * @throws IOException 抛IO异常
+     */
+    @GetMapping("/list/export")
+    public void excelDownload(@RequestParam("userId") Long userId ,HttpServletResponse response) throws Exception {
+
+        List<List<String>> excelData = new ArrayList<>();
+
+        //查询数据
+        Result<List<Device>> res = deviceService.findAllByUser(userId);
+        //数据下载
+        deviceService.excelDownload(userId,res,response);
+    }
+
+
 }
--- src/main/java/com/hwcao/iot/service/DeviceService.java
+++ src/main/java/com/hwcao/iot/service/DeviceService.java
@@ -4,6 +4,7 @@ import com.hwcao.iot.dto.device.DeviceReqDTO;
 import com.hwcao.iot.entity.Result;
 import com.hwcao.iot.entity.device.Device;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface DeviceService {
@@ -17,4 +18,6 @@ public interface DeviceService {
     Result deleteDevice(Long id);
 
     Result<List<Device>> findAllByUser(Long userId);
+
+    void excelDownload(Long userId,Result<List<Device>> res, HttpServletResponse response) throws Exception;
 }
