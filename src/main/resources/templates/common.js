778d3bc6624cbe2f802902737b4219b4215409cf
dyingstraw <1083608577@qq.com>
2019-07-09 14:36:14 +0800

    添加前端页面（由于前端不是本项目主要部分，简化前端路由和显示逻辑）

---
+++ src/main/resources/templates/common.js
@@ -0,0 +1,34 @@
+
+
+function ddd(){
+    console.log("mylib is init!")
+    this.mainActiveIndex =null;
+
+}
+ddd.prototype.handleSelect = function (key, keyPath) {
+    console.log(key);
+    console.log(keyPath);
+    temp = key;
+    if (key == "1") {
+        window.location.href = 'index.html';
+    } else if (key == "3") {
+        window.location.href = 'records.html';
+    } else if (key == "4") {
+        window.location.href = 'applation.html';
+    } else {
+        if (key == "2-1") {
+            window.location.href = 'addDevice.html';
+        } else if (key == "2-2") {
+            window.location.href = 'device.html';
+
+        } else if (key == "2-3") {
+            window.location.href = 'realControl.html';
+        }
+    }
+}
+ddd.prototype.mainActiveIndex =function(){
+    return temp;
+}
+$dsc=function $dsc(){
+    return new ddd();
+}
